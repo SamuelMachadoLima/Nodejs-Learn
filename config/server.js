42af303e297a4e96db5a87e63862d7efbffc75e9
samuel-machado <devsamuelm@gmail.com>
2020-07-01 17:56:56 -0300

    Express-Validator para validação do formulário

--- config/server.js
+++ config/server.js
@@ -2,12 +2,14 @@
 var app = require('express')();
 var consign = require('consign');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
 // INCORPORANDO O EJS JUNTO AO EXPRESS ATRAVES DE UM MÉTODO
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressValidator());
 
 consign()
     .include('app/routes')
